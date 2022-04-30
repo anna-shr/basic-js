fe9190281b2d72b4b24e6a84b4f5ed0ef9899b3b
Anna Sheveleva <anna.girl.v@mail.ru>
2022-04-30 19:14:01 +0300

    dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,22 +1,25 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function createDreamTeam(array) {
+  if (Array.isArray(array) == false) {
+    return false;
+  }
+
+  let result = [];
+  for (i = 0; i < array.length; i++) {
+    if (typeof (array[i]) === 'string') {
+      let res = array[i].trim().split('');
+      result.push(res[0].toUpperCase());
+    }
+  }
+  result = result.sort();
+  return result.toString().replace(/[\s.,%]/g, '');
 }
+
+
 
 module.exports = {
   createDreamTeam
