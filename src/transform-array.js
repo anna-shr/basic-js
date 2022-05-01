c60615ea4832beb749080aef94a064f2aea65ee4
Anna Sheveleva <anna.girl.v@mail.ru>
2022-05-01 12:58:12 +0300

    transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,10 +15,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(array) {
+  if (Array.isArray(array) == false) {
+    throw new NotImplementedError("'arr' parameter must be an instance of the Array!");
+  }
+
+  let arr = array.slice(0);
+  let ind;
+
+  if (ind = arr.findIndex(i => i === '--discard-prev')) {
+    if (arr[ind] == arr[0]) {
+      arr = arr.splice(arr[0], 1);
+      return arr;
+    } else {
+    let removed = arr.splice(ind - 1, 2);
+    return arr;
+  }
+
+  if (ind = arr.findIndex(i => i === '--discard-next')) {
+    if (arr[ind] == arr[arr.length - 1]) {
+      arr = arr.splice(arr[arr.length - 1]);
+      return arr;
+    } else {
+      let removed = arr.splice(ind, 2);
+      return arr;
+    }
+  }
+
+  if (ind = arr.findIndex(i => i === '--double-prev')) {
+      let removed = arr.splice(ind, 1, ind);
+      return arr;
+    }
+  }
+
+  if (ind = arr.findIndex(i => i === '--double-next')) {
+    let removed = arr.splice(ind, 1, ind + 1);
+    return arr;
+  }
 }
+
+
+
+
+
+/*let arr = [1,2,3, 'deleteprev', 4,5,6];
+console.log(arr);
+let ind = arr.findIndex(i=>i==='--double-next');
+arr.splice(ind-1,2);
+concat();
+console.log(arr); // 12'deleteprev'456*/
+
 
 module.exports = {
   transform
